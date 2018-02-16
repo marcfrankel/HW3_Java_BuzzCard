a42d8fe811db1fcd83196a4a6ff3c78608b95280
Marc Frankel <mfrankel218@gmail.com>
2018-02-15 23:43:07 -0500

    Completed javadocs checkstyle

--- BuzzCard.java
+++ BuzzCard.java
@@ -10,38 +10,72 @@ public class BuzzCard {
     private double diningDollars;
     private double buzzFunds;
 
+    /**
+     * Buzzcard constructor
+     * @param mealSwipes Amount of mealswipes on card as int
+     * @param diningDollars Amount of dining Dollars as double
+     * @param buzzFunds Amount of buzz funds as double
+     */
     public BuzzCard(int mealSwipes, double diningDollars, double buzzFunds) {
         this.mealSwipes = mealSwipes;
         this.diningDollars = diningDollars;
         this.buzzFunds = buzzFunds;
     }
 
+    /**
+     * toString function for Buzzcard
+     * @return string representation of card values
+     */
     public String toString() {
         return String.format("Buzzcard balance with Dining Dollars: %s,"
             + " BuzzFunds: %s, Meal Swipes: %s",
             diningDollars, buzzFunds, mealSwipes);
     }
 
+    /**
+     * mealSwipes setter method
+     * @param mealSwipes New value to set meal swipes to
+     */
     public void setMealSwipes(int mealSwipes) {
         this.mealSwipes = mealSwipes;
     }
 
+    /**
+     * mealSwipes getter method
+     * @return current meal swipes
+     */
     public int getMealSwipes() {
         return mealSwipes;
     }
 
+    /**
+     * diningDollars setter method
+     * @param diningDollars New value to set dinning dollars to
+     */
     public void setDiningDollars(double diningDollars) {
         this.diningDollars = diningDollars;
     }
 
+    /**
+     * diningDollars getter method
+     * @return current diningDollars
+     */
     public double getDiningDollars() {
         return diningDollars;
     }
 
+    /**
+     * buzzFunds setter method
+     * @param buzzFunds New value to set buzz funds to
+     */
     public void setBuzzFunds(double buzzFunds) {
         this.buzzFunds = buzzFunds;
     }
 
+    /**
+     * buzzFunds getter method
+     * @return current buzzFunds
+     */
     public double getBuzzFunds() {
         return buzzFunds;
     }
--- Student.java
+++ Student.java
@@ -9,25 +9,46 @@ public class Student {
     private String name;
     private int id;
 
-
+    /**
+     * Student constructor method
+     * @param card A BuzzCard object for the student
+     * @param name A name for the student as a string
+     * @param id A GTID for the student as an int
+     */
     public Student(BuzzCard card, String name, int id) {
         this.card = card;
         this.name = name;
         this.id = id;
     }
 
+    /**
+     * card getter method
+     * @return the object's BuzzCard object
+     */
     public BuzzCard getBuzzCard() {
         return card;
     }
 
+    /**
+     * name getter method
+     * @return the name of the student
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * id getter method
+     * @return id of the student
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * simple function to buy a brittain item on meal swipes
+     * @param item The BrittainItem you wish to buy
+     */
     public void buyBrittainMealSwipes(BrittainItem item) {
         int cost = item.getMealSwipes();
         int currentSwipes = card.getMealSwipes();
@@ -39,6 +60,10 @@ public class Student {
         }
     }
 
+    /**
+     * simple function to buy a brittain item on buzz funds
+     * @param item The BrittainItem you wish to buy
+     */
     public void buyBrittainBuzzFunds(BrittainItem item) {
         double cost = item.getBuzzFunds();
         double currentFunds = card.getBuzzFunds();
@@ -50,6 +75,10 @@ public class Student {
         }
     }
 
+    /**
+     * simple function to buy a burdells item on buzz funds
+     * @param item The BurdellsItem you wish to buy
+     */
     public void buyBurdellsBuzzFunds(BurdellsItem item) {
         double cost = item.getBuzzFunds();
         double currentFunds = card.getBuzzFunds();
@@ -61,6 +90,10 @@ public class Student {
         }
     }
 
+    /**
+     * simple function to buy a SubwayItem item on dining dollars
+     * @param item The SubwayItem you wish to buy
+     */
     public void buySubwayDiningDollars(SubwayItem item) {
         double cost = item.getDiningDollars();
         double currentDiningDollars = card.getDiningDollars();
@@ -72,6 +105,10 @@ public class Student {
         }
     }
 
+    /**
+     * simple function to buy a SubwayItem item on buzz funds
+     * @param item The SubwayItem you wish to buy
+     */
     public void buySubwayBuzzFunds(SubwayItem item) {
         double cost = item.getBuzzFunds();
         double currentFunds = card.getBuzzFunds();
@@ -83,6 +120,10 @@ public class Student {
         }
     }
 
+    /**
+     * Student toString() function
+     * @return a string representation of the student
+     */
     public String toString() {
         return String.format("Student named %s"
         + " with ID: %s %s", name, id, card.toString());
